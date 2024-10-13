5e68cb5f08133a2a2c67d774b38e9c5068ff2b66
sigmachebypel228 <timuryablokov2012@gmail.com>
2024-10-13 18:35:28 +0300

    Вот дз

---
+++ hb/main_2.py
@@ -0,0 +1,13 @@
+from hitbox import Hitbox
+
+hb1 = Hitbox(0, 100, 100, 100)
+hb2 = Hitbox(110, 100, 100, 100)
+hb3 = Hitbox(220, 100, 100, 100)
+
+intersection1 = hb1.intersects(hb2)
+intersection2 = hb2.intersects(hb3)
+intersection3 = hb1.intersects(hb3)
+
+print(intersection1)
+print(intersection2)
+print(intersection3)
